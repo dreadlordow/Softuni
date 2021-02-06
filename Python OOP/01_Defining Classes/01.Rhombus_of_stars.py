9b5888ab339941c6cb2f64377152ebdca2ff8d92
Georgi <georgipavlov1913@gmail.com>
2021-02-06 13:00:00 +0100

    Defining classes

---
+++ Python OOP/01_Defining Classes/01.Rhombus_of_stars.py
@@ -0,0 +1,14 @@
+GROWING = 1
+SHRINKING = -1
+def print_rhombus(n):
+    def print_line(i, direction):
+        if i == 0:
+            return
+        line = ' ' * (n - i) + '* ' * i
+        print(line.rstrip())
+        if i == n:
+            direction = SHRINKING
+        print_line(i + direction, direction)
+    print_line(1, GROWING)
+
+print_rhombus(int(input()))
